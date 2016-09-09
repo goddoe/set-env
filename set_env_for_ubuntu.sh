1d55b791a0d512c7099b321cecd819eea5d3dd64
Sung-ju Kim <goddoe2@gmail.com>
2016-09-09 18:49:15 +0900

    add virtuanenv

--- set_env_for_ubuntu.sh
+++ set_env_for_ubuntu.sh
@@ -29,3 +29,5 @@ vim +PluginInstall +qall
 sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s /usr/bin/zsh
+
+sudo pip3 install virtualenv
