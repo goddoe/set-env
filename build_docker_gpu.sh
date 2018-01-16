64125c839ba382d940d234f9bc0b84788158184c
Sung-ju Kim <goddoe2@gmail.com>
2018-01-16 10:11:17 +0900

    update Docker file

--- build_docker_gpu.sh
+++ build_docker_gpu.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-nvidia-docker build -f ./Dockerfile.gpu --build-arg cuda_version=9.0 --build-arg cuda_version=7 -t dev/gpu:1.0 .
+nvidia-docker build -f ./Dockerfile.gpu --build-arg cuda_version=8.0 --build-arg cuda_version=5 -t dev/gpu:1.0 .
