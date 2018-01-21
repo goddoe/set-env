4c2b41942d72134981c059a834ec56283273900c
Sung-ju Kim <goddoe@goddoe2@gmail.com>
2018-01-21 20:49:01 +0900

    update

--- set_env_for_ubuntu.sh
+++ set_env_for_ubuntu.sh
@@ -3,15 +3,18 @@
 # install develop env
 apt-get install -y sudo
 sudo apt-get update -y
-sudo apt-get install -y build-essential git pkg-config ctags libjemalloc1 screen curl cmake
+sudo apt-get install -y build-essential git pkg-config ctags libjemalloc1 screen curl cmake software-properties-common
 
 # install vim and vim setup
-sudo apt-get install -y vim-gnome
+# sudo apt-get install -y vim-gnome
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt update
+sudo apt install vim
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cat ./vimrc >> ~/.vimrc
 vim +PluginInstall +qall
 
-export python=python3
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 
 # set screen config
@@ -24,7 +27,11 @@ sudo pip3 install -r ./requirements.txt
 # install zsh and oh-my-zsh
 sudo apt-get install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s /usr/bin/zsh
+
+# virtualenvwrapper
+# export WORKON_HOME=~/envs
+echo 'export VIRTUALENVWRAPPER_PYTHON=`which python3`' >> ~/.zshrc
+echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.zshrc
 
 # java
 sudo apt-get install -y default-jdk
@@ -33,3 +40,6 @@ if [ -z "$JAVA_HOME" ]; then
     echo "export JAVA_HOME=/usr/lib/jvm/default-java" >> ~/.bashrc
     echo "export JAVA_HOME=/usr/lib/jvm/default-java" >> ~/.zshrc
 fi
+
+# set zsh to default
+chsh -s /usr/bin/zsh
