e926606a38d59bec9937129af4c62ae3c9c53ed0
Sung-ju Kim <goddoe2@naver.com>
2019-06-30 17:50:06 +0900

    Update set_env_for_ubuntu.sh

--- set_env_for_ubuntu.sh
+++ set_env_for_ubuntu.sh
@@ -50,3 +50,6 @@ fi
 
 # set zsh to default
 chsh -s /usr/bin/zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
